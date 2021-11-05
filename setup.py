8459a3ebc624a9d7217e86e8681b274228b68872
Edderic Ugaddan <eugaddan@panoramaed.com>
2021-11-05 08:49:26 -0400

    upgrade version to 0.4.0

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="unlikely",
-    version="0.3.0",
+    version="0.4.0",
     author="Edderic Ugaddan",
     author_email="edderic@gmail.com",
     description="Parallelized, Likelihood-free Bayesian Inference",
